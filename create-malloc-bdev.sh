ce415db6055f3f39b55a0209aa43fcb3789fb883
Serg Kryvonos <sergeikrivonos@gmail.com>
2025-01-24 14:26:18 -0500

    create-malloc-bdev.sh: output name of new nvme block device

--- create-malloc-bdev.sh
+++ create-malloc-bdev.sh
@@ -2,30 +2,34 @@
 set -x
 SPDK_PATH=$(realpath ~/spdk)
 
+ls /dev/nvme* > _/nvme.lst.bak
+
 # Exit immediately if a command exits with a non-zero status
 set -e
 
 SPDK_RPC=$SPDK_PATH/scripts/rpc.py
 
 echo "Creating malloc bdev using $SPDK_RPC"
-sudo $SPDK_RPC bdev_malloc_create -b Malloc0 1024 512 || echo "Malloc bdev already exists"
+sudo $SPDK_RPC bdev_malloc_create -b Malloc0 1024 512 || echo "Error Creating malloc bdev"
 
 echo "Creating pass-through bdev on top of malloc bdev using $SPDK_RPC"
-sudo $SPDK_RPC bdev_passthru_create -b PTBdev0 -p Malloc0 || echo "Pass-through bdev already exists"
+sudo $SPDK_RPC bdev_passthru_create -b PTBdev0 -p Malloc0 || echo "Error Creating pass-through bdev"
 
 echo "Creating NVMf transport"
-sudo $SPDK_RPC nvmf_create_transport -t TCP -u 8192 || echo "NVMf transport already exists"
+sudo $SPDK_RPC nvmf_create_transport -t TCP -u 8192 || echo "Error Creating NVMf transport"
 
 echo "Creating NVMf subsystem and namespace using $SPDK_RPC"
-sudo $SPDK_RPC nvmf_create_subsystem nqn.2023-01.io.spdk:cnode1 -a -s SPDK00000000000001 || echo "NVMf subsystem already exists"
+sudo $SPDK_RPC nvmf_create_subsystem nqn.2023-01.io.spdk:cnode1 -a -s SPDK00000000000001 || echo "Error Creating NVMf subsystem"
 
 echo "Creating NVMf subsystem and namespace using $SPDK_RPC"
-sudo $SPDK_RPC nvmf_subsystem_add_ns nqn.2023-01.io.spdk:cnode1 PTBdev0 || echo "NVMf namespace already exists"
+sudo $SPDK_RPC nvmf_subsystem_add_ns nqn.2023-01.io.spdk:cnode1 PTBdev0 || echo "Error Adding NVMf namespace"
 
 echo "Creating NVMf subsystem and namespace using $SPDK_RPC"
-sudo $SPDK_RPC nvmf_subsystem_add_listener nqn.2023-01.io.spdk:cnode1 -t tcp -a 127.0.0.1 -s 4420 || echo "NVMf listener already exists"
+sudo $SPDK_RPC nvmf_subsystem_add_listener nqn.2023-01.io.spdk:cnode1 -t tcp -a 127.0.0.1 -s 4420 || echo "Error Adding NVMf listener"
 
 echo "Connecting to NVMf subsystem locally using $SPDK_RPC"
-sudo nvme connect -t tcp -n nqn.2023-01.io.spdk:cnode1 -a 127.0.0.1 -s 4420 || echo "NVMf connection already exists"
+sudo nvme connect -t tcp -n nqn.2023-01.io.spdk:cnode1 -a 127.0.0.1 -s 4420 || echo "Error Connecting to NVMf subsystem"
 
 echo "Configuration complete."
+ls /dev/nvme* > _/nvme.lst
+diff _/nvme.lst _/nvme.lst.bak
